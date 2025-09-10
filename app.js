01731373399792e5eccba566884802c41e43519e
Osarenoma Blessing Dick <blessingosarenoma@gmail.com>
2025-09-10 15:18:59 +0100

    objects

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("happy!");
+
+const emotion = {
+  name: "Bob",
+  age: 30,
+  favouriteColour: "red",
+  sayHello: function () {
+    console.log("Hello!");
+  },
+};
+emotion.sayHello();
